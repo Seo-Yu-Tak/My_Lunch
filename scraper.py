c6960234e10f8eeb067b4a31ae9f009182a0365d
yutak <soano2428@gmail.com>
2023-05-14 17:55:39 +0900

    v1

---
+++ scraper.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+url = "https://www.gnu.ac.kr/main/ad/fm/foodmenu/selectFoodMenuView.do?mi=1341&restSeq=5"
+
+page = requests.get(url)
+soup = BeautifulSoup(page.text, "html.parser")
+
+elements = soup.select('#detailForm div table tbody tr:nth-child(2) p')
+
+
+menu_list = []
+for i in range(len(elements)):
+    menu_list.append(elements[i].get_text())
+    print(elements[i])
+    
+#print(menu_list[0])
